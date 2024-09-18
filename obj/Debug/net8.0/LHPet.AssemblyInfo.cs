402dc9440c2bb5d964b8f277d9eea92fd52618e1
Jefte <jeftefontes@gmail.com>
2024-09-18 02:24:09 -0300

    .

--- obj/Debug/net8.0/LHPet.AssemblyInfo.cs
+++ obj/Debug/net8.0/LHPet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LHPet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5971f795d66cb9a19f6080df4462ea32a2ab24c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LHPet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LHPet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
